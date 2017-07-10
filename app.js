255d60a29357e3415d7f1bbcea772a76e45cb9c6
Steve Kamal <kamal.steve@gmail.com>
2017-07-10 06:50:06 -0400

    nodemon and routed to hello page

--- app.js
+++ app.js
@@ -2,9 +2,15 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request,response) => {
-    response.send('ayy bb');
+app.get('/', (req,res) => {
+    res.send('<h1>I love Treehouse!</h1>');
 });
 
-app.listen(3000);
+app.get('/hello', (req,res) => {
+    res.send('<h1>Hello BOI!</h1>');
+});
+
+app.listen(3000, () =>{
+    console.log('The application is running on localhost:3000')
+});
 
