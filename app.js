05391b917020050fb0d1727d87b38c593d4dc52c
Steve Kamal <kamal.steve@gmail.com>
2017-07-10 15:33:38 -0400

    redirects

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const pug = require('pug');
 
 
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(cookieParser());
 app.set('view engine','pug');
 
 app.get('/', (req,res) => {
-    res.render('index');
+    const name = req.cookies.username
+    res.render('index',{name});
+});
+
+app.get('/hello', (req,res) => {
+    res.render('hello');
+});
+app.post('/hello', (req,res) => {
+    res.cookie('username',req.body.username)
+    res.redirect('/');
 });
 
 app.get('/cards', (req,res) => {
