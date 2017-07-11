eb50edaf4b62c1e0c21d2da9fa49b3d41a25676f
Steve Kamal <kamal.steve@gmail.com>
2017-07-10 21:37:38 -0400

    error handling middleware

--- app.js
+++ app.js
@@ -10,12 +10,14 @@ app.use(cookieParser());
 app.set('view engine','pug');
 
 app.use((req,res,next) =>{
-    req.message = 'This message made it!';
-    next();
+    console.log('hello')
+    const err = new Error('Oh noes!')
+    err.status = 500;
+    next(err);
 });
 
 app.use((req,res,next) =>{
-    console.log(req.message);
+    console.log('World');
     next();
 });
 
@@ -50,6 +52,13 @@ app.post('/goodbye', (req,res) => {
     res.clearCookie('username')
     res.redirect('/hello');
 });
+
+app.use((err,req,res,next)=> {
+    res.locals.error = err;
+    res.status(err.status);
+    res.render('error')
+
+})
 
 app.listen(3000, () =>{
     console.log('The application is running on localhost:3000')
